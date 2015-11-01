6e28a64d22ffa50db327e60dc6400c108ea2840d
gojira <jason@theograys.com>
2015-11-01 08:44:36 -0500

    go fmt.

--- lib/task_test.go
+++ lib/task_test.go
@@ -3,7 +3,7 @@ package lib
 import (
 	"os"
 	"testing"
-  "time"
+	"time"
 )
 
 func buildTask() Task {
@@ -39,29 +39,29 @@ func TestExecuteIfThis(t *testing.T) {
 }
 
 func TestExecuteThenThat(t *testing.T) {
-  task := buildTask()
-  success := task.ExecuteIfThis()
-  if !success {
-    t.Error("Failed to create x.test for testing")
-  } else {
-    thenThatSuccess := task.ExecuteThenThat()
-    _, err := os.Stat("x.test")
-    if os.IsExist(err) || !thenThatSuccess {
-      t.Error("ExecuteThenThat failed to clean up x.test")
-    }
-  }
+	task := buildTask()
+	success := task.ExecuteIfThis()
+	if !success {
+		t.Error("Failed to create x.test for testing")
+	} else {
+		thenThatSuccess := task.ExecuteThenThat()
+		_, err := os.Stat("x.test")
+		if os.IsExist(err) || !thenThatSuccess {
+			t.Error("ExecuteThenThat failed to clean up x.test")
+		}
+	}
 }
 
 func TestSleepNow(t *testing.T) {
-  task := buildTask()
-  start := time.Now()
-  task.SleepNow()
-  duration := time.Since(start)
-  seconds := duration.Seconds()
-  if seconds < 2.0 {
-    t.Error("Sleep did not sleep long enoug:", seconds)
-  }
-  if seconds > 2.5 {
-    t.Error("Sleep went for too long:", seconds)
-  }
+	task := buildTask()
+	start := time.Now()
+	task.SleepNow()
+	duration := time.Since(start)
+	seconds := duration.Seconds()
+	if seconds < 2.0 {
+		t.Error("Sleep did not sleep long enoug:", seconds)
+	}
+	if seconds > 2.5 {
+		t.Error("Sleep went for too long:", seconds)
+	}
 }
